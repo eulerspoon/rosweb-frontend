74f0d1243f26a28773e817160548ef1a759b87ca
solit16 <alexnoskov_44@mail.ru>
2025-12-08 19:50:43 +0300

    video added, filter bug fixed

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,51 +1,56 @@
 import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
+import './MainPage.css';
 
 const MainPage: React.FC = () => {
   return (
-    <Container>
-      <Row className="my-5">
-        <Col>
-          <h1 className="text-center mb-4">Добро пожаловать в ROSWeb</h1>
-          <p className="text-center lead">
-            Система управления командами ROS для робототехники
-          </p>
-        </Col>
-      </Row>
+    <div className="main-page-container">
+      <div className="background-gif"></div>
       
-      <Row>
-        <Col md={4}>
-          <Card className="h-100">
-            <Card.Body>
-              <Card.Title>Управление командами</Card.Title>
-              <Card.Text>
-                Создавайте команды для управления роботом
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4}>
-          <Card className="h-100">
-            <Card.Body>
-              <Card.Title>Планирование маршрутов</Card.Title>
-              <Card.Text>
-                Формируйте маршруты из последовательности команд
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4}>
-          <Card className="h-100">
-            <Card.Body>
-              <Card.Title>Визуализация результата</Card.Title>
-              <Card.Text>
-                Отслеживайте положение робота по завершении маршрута
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+      <Container className="content-overlay">
+        <Row className="my-5">
+          <Col>
+            <h1 className="text-center mb-4 text-white">Добро пожаловать в ROSWeb</h1>
+            <p className="text-center lead text-white">
+              Система управления командами ROS для робототехники
+            </p>
+          </Col>
+        </Row>
+        
+        <Row>
+          <Col md={4}>
+            <Card className="h-100 card-transparent">
+              <Card.Body>
+                <Card.Title>Управление командами</Card.Title>
+                <Card.Text>
+                  Создавайте команды для управления роботом
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4}>
+            <Card className="h-100 card-transparent">
+              <Card.Body>
+                <Card.Title>Планирование маршрутов</Card.Title>
+                <Card.Text>
+                  Формируйте маршруты из последовательности команд
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4}>
+            <Card className="h-100 card-transparent">
+              <Card.Body>
+                <Card.Title>Визуализация результата</Card.Title>
+                <Card.Text>
+                  Отслеживайте положение робота по завершении маршрута
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
